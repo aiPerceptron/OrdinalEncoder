796c143f6682bd292afdefe13980d955c741b24d
aiPerceptron <193553617+aiPerceptron@users.noreply.github.com>
2025-02-27 19:06:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ This program is about converting text to numbers in scikitlearn.
 NOTE: this is not machine learning, the ordinal encoder will not guess 
 If a word not shown in the training data shows up in the testing data, it will not work.
 
+This is a four step process.
 """
 
 from sklearn.preprocessing import OrdinalEncoder
 import numpy as np
 
-# getting the data
+# Step 1: getting the data
 
 training = np.array(["bye","hello","maybe"]).reshape([-1,1]) # the reshape converts to 2d array with a single column.
 
@@ -17,15 +18,15 @@ testing1 = np.array(["bye", "hello", "hello"]).reshape([-1,1])
 
 testing2 = [["hello"],["bye"]]
 
-# choose your transformer
+# Step 2: choose your transformer
 
 OE = OrdinalEncoder()
 
-# fitting your transformer of choice with the training data
+# Step 3: fitting your transformer of choice with the training data
 
 OE.fit(training)
 
-# transform all the data, including the testing data
+# Step 4: transform all the data, including the testing data
 
 OE_training = OE.transform(training)
 OE_testing1 = OE.transform(testing1)
